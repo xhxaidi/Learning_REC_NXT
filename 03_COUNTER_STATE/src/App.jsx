4ad00969c35889556908367745da628003bc4f9b
Syed Hasan Ifham Zaidi <119783825+xhxaidi@users.noreply.github.com>
2026-05-06 01:21:29 +0530

    module 03

---
+++ 03_COUNTER_STATE/src/App.jsx
@@ -0,0 +1,12 @@
+import"./App.css";
+
+const App = () => {
+  return (
+    <>
+    <h1>Let's build our first project in react</h1>
+    <p>I am ready for this</p>
+    </>
+  )
+}
+
+export default App
